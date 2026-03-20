dcd1c82f1f460f5571a71ab257e480b21b4f8da2
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-03-20 16:07:05 +0200

    Initial commit

---
+++ src/test/java/org/example/eventregistrationsystem/EventRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eventregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
